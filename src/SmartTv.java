99411b75a3e68ab98d57b67e4be5a6110f7d4df8
MarcellyaAlves <marcelly_figueiredo@hotmail.com>
2023-01-31 12:49:56 -0300

    smart Tv atualizada

--- src/SmartTv.java
+++ src/SmartTv.java
@@ -1,5 +1,35 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void mudarCanal(int novoCanal) {
+        canal = novoCanal;
+    }
+
+    public void aumentarCanal() {
+        canal++;
+    }
+
+    public void diminuirCanal() {
+        canal--;
+    }
+
+    public void aumentarVolume() {
+        volume++; // volume = volume + 1
+        System.out.println("Aumentando o volume para: " + volume);
+    }
+
+    public void diminuirVolume() {
+        volume--; // volume = volume - 1
+        System.out.println("Diminuindo o volume para: " + volume);
+    }
+
+    public void ligar() {
+        ligada = true;
+    }
+
+    public void desligar() {
+        ligada = false;
     }
 }
